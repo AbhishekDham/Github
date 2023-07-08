e356e3f174a0ff1de325ee836f38f1f8f0da6dd9
Abhishek <Abhishek@DESKTOP-JQK94OT>
2023-07-09 00:09:46 +0530

    Test

---
+++ Github/src/Assignment/Method.java
@@ -0,0 +1,31 @@
+package Assignment;
+
+public class Method {
+public static void main(String[] args) {
+	
+}
+}
+
+class NonParameterized{
+	
+	// Static method
+	
+	static int abc(){
+		int A =10;
+		return A;
+		
+		
+	}
+	
+	// Non static method
+	
+	String name(){
+		String A ="Test";
+		return A;
+		
+		
+	}
+	
+	
+	
+}
